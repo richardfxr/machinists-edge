cf82a1bc9b7be3de08266642937a6931154e3349
Richard Fu <richardfxr.personal@gmail.com>
2022-10-21 22:48:13 -0400

    💄CSS: added basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<main id="main">
+    <slot></slot>
+</main>
+
+
+<style lang="scss">
+    #main {
+        max-width: var(--max-width);
+        margin: auto;
+        background-color: pink;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,107 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<form action="" class="feedCalc">
+    <div class="inputs">
+
+    </div>
+
+    <div class="alwaysVisible">
+        <div class="alwaysVisible__inner">
+
+        </div>
+    </div>
+
+    <div class="results">
+        <div class="results__inner">
+
+        </div>
+    </div>
+</form>
+
+
+<style lang="scss">
+    .feedCalc {
+        // internal variables
+        --_results-width: 400px;
+        --_alwaysVisible-height: 80px;
+
+        display: grid;
+        grid-template-columns: var(--_results-width) auto;
+        grid-template-areas: "results inputs";
+        align-items: start;
+        position: relative;
+
+        margin-top: 100px;
+        margin-bottom: 100px;
+        
+        .inputs {
+            grid-area: inputs;
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            height: 300vh;
+
+            background-color: salmon;
+        }
+
+        .alwaysVisible {
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+
+            &__inner {
+                position: sticky;
+                top: 0;
+                left: 0;
+                z-index: 100;
+                width: var(--_results-width);
+                height: var(--_alwaysVisible-height);
+
+                background-color: red;
+            }
+        }
+
+        .results {
+            grid-area: results;
+            position: sticky;
+            top: var(--_alwaysVisible-height);
+            max-height: calc(100vh - var(--_alwaysVisible-height));
+
+            margin-top: var(--_alwaysVisible-height);
+            background-color: aquamarine;
+            overflow-y: auto;
+
+            &__inner {
+                height: 600px;
+                background-color: aqua;
+            }
+        }
+    }
+
+    /* === BREAKPOINTS ======================== */
+    @media only screen and (max-width: $breakpoint-mobile) {
+        .feedCalc {
+            display: flex;
+            flex-direction: column-reverse;
+
+            .inputs {
+                grid-area: unset;
+                width: 100%; 
+            }
+
+            .alwaysVisible {
+                right: 0;
+
+                &__inner {
+                    width: 100%;
+                }
+            }
+
+            .results {
+                grid-area: unset;
+                position: relative;
+                top: 0;
+                width: 100%;
+                max-height: unset;
+            }
+        }
+    }
+</style>
