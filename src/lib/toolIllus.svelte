2f5ad2475d51e7e09babf47153f3b197b08afc39
Richard Fu <richardfxr.personal@gmail.com>
2022-10-22 22:12:39 -0400

    🟠SVLT: created toolIllus & added types

---
+++ src/lib/toolIllus.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+    /* === IMPORTS ============================ */
+    import ToolHolder from "$lib/SVGs/toolHolder.svelte";
+    import BitTop from "$lib/SVGs/bit-top.svelte";
+    import BitBoundingLines from "$lib/SVGs/bit-boundingLines.svelte";
+    import FluteFront from "$lib/SVGs/flute-front.svelte";
+    import FluteBack from "$lib/SVGs/flute-back.svelte";
+    import FluteCenter from "$lib/SVGs/bit-center.svelte";
+
+    /* === PROPS ============================== */
+    export let scaleX: number;
+    export let flutes: number;
+
+    /* === FUNCTIONS ========================== */
+    function clamp(input: number, min: number, max: number): number {
+        return Math.min(Math.max(input, min), max);
+    }
+
+    /* === REACTIVE DECLARATIONS ============== */
+    $: displayedFlutes = clamp(flutes, 1, 5);
+    $: displayedScaleX = clamp(scaleX, 0.2, 1);
+</script>
+
+
+<tool-illus class="tool__illus">
+    <ToolHolder />
+    <div class="bit" style="--scaleX: {displayedScaleX}; --flutes: {displayedFlutes};">
+        <BitTop scaleX={displayedScaleX} />
+        <div class="bit__bottom">
+            <div class="flutes">
+                <div class="flutes__inner">
+                    {#each Array(displayedFlutes) as _, i}
+                        <FluteBack
+                            scaleX={displayedScaleX}
+                            position={i === 0 ? "relative" : "absolute"}
+                            zIndex={2 * i}
+                            index={0}
+                            offsetY={(50 / displayedFlutes) * -i} />
+                        <FluteFront
+                            scaleX={displayedScaleX}
+                            position={i === 0 ? "relative" : "absolute"}
+                            zIndex={2 * i + 100}
+                            index={1}
+                            offsetY={(50 / displayedFlutes) * -i} />
+                        <FluteBack
+                            scaleX={displayedScaleX}
+                            position={i === 0 ? "relative" : "absolute"}
+                            zIndex={2 * i + 1}
+                            index={2}
+                            offsetY={(50 / displayedFlutes) * -i} />
+                        <FluteFront
+                            scaleX={displayedScaleX}
+                            position={i === 0 ? "relative" : "absolute"}
+                            zIndex={2 * i + 101}
+                            index={3}
+                            offsetY={(50 / displayedFlutes) * -i} />
+                    {/each}
+                    <FluteCenter scaleX={displayedScaleX} zIndex={99} />
+                </div>
+            </div>
+            <BitBoundingLines scaleX={displayedScaleX} />
+        </div>
+    </div>
+</tool-illus>
+
+
+<style lang="scss">
+    .tool__illus {
+        // internal varaibles
+        --_shoulder-height: 20px;
+        --_flute-height: 240px;
+
+        display: flex;
+        flex-direction: column;
+        position: relative;
+
+        padding: 0 24%;
+
+        &::before {
+            // gradient overlay for tool illustration
+            content: "";
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            z-index: 2;
+
+            opacity: 0.05;
+            background: linear-gradient(var(--clr-900), var(--clr-100) 90%);
+        }
+
+        .bit {
+            position: relative;
+            width: 100%;
+
+            padding: 0 32%;
+
+            // transform: translateX(calc(50% - (var(--scaleX) / 2) * 100%));
+
+            :global(.illustration) {
+                transform: translateX(calc(50% - (var(--scaleX) / 2) * 100%));
+            }
+
+            &__bottom {
+                position: relative;
+            }
+
+            .flutes {
+                position: absolute;
+                top: 0;
+                right: 0;
+                bottom: 0;
+                left: 0;
+
+                overflow: hidden;
+
+                &__inner {
+                    position: relative;
+                    animation: moveUp 2s linear infinite;
+                }
+            }
+        }
+    }
+
+    /* === ANIMATIONS ========================= */
+    @keyframes moveUp {
+        from { transform: translateY(0%); }
+        to { transform: translateY(calc(-50% / var(--flutes))); }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,6 @@
-<script>
+<script lang="ts">
     /* === IMPORTS ============================ */
-    import ToolHolder from "$lib/SVGs/toolHolder.svelte";
-    import BitTop from "$lib/SVGs/bit-top.svelte";
-    import BitBoundingLines from "$lib/SVGs/bit-boundingLines.svelte";
-    import FluteFront from "$lib/SVGs/flute-front.svelte";
-    import FluteBack from "$lib/SVGs/flute-back.svelte";
-    import FluteCenter from "$lib/SVGs/bit-center.svelte";
+    import ToolIllus from "$lib/toolIllus.svelte";
 
     /* === VARAIBLES ========================== */
     let scaleX = 1;
@@ -26,46 +21,7 @@
 
     <div class="results">
         <div class="results__inner">
-            <div class="tool__illustration">
-                <ToolHolder />
-                <div class="bit" style="--scaleX: {scaleX}; --flutes: {flutes};">
-                    <BitTop {scaleX} />
-                    <div class="bit__bottom">
-                        <div class="flutes">
-                            <div class="flutes__inner">
-                                {#each Array(flutes) as _, i}
-                                    <FluteBack
-                                        {scaleX}
-                                        position={i === 0 ? "relative" : "absolute"}
-                                        zIndex={2 * i}
-                                        index={0}
-                                        offsetY={(50 / flutes) * -i} />
-                                    <FluteFront
-                                        {scaleX}
-                                        position={i === 0 ? "relative" : "absolute"}
-                                        zIndex={2 * i + 100}
-                                        index={1}
-                                        offsetY={(50 / flutes) * -i} />
-                                    <FluteBack
-                                        {scaleX}
-                                        position={i === 0 ? "relative" : "absolute"}
-                                        zIndex={2 * i + 1}
-                                        index={2}
-                                        offsetY={(50 / flutes) * -i} />
-                                    <FluteFront
-                                        {scaleX}
-                                        position={i === 0 ? "relative" : "absolute"}
-                                        zIndex={2 * i + 101}
-                                        index={3}
-                                        offsetY={(50 / flutes) * -i} />
-                                {/each}
-                                <FluteCenter {scaleX} zIndex={99} />
-                            </div>
-                        </div>
-                        <BitBoundingLines {scaleX} />
-                    </div>
-                </div>
-            </div>
+            <ToolIllus {scaleX} {flutes} />
         </div>
     </div>
 </form>
@@ -128,64 +84,6 @@
             &__inner {
                 height: 600px;
             }
-
-            .tool__illustration {
-                // internal varaibles
-                --_shoulder-height: 20px;
-                --_flute-height: 240px;
-
-                display: flex;
-                flex-direction: column;
-                position: relative;
-
-                padding: 0 24%;
-
-                &::before {
-                    // gradient overlay for tool illustration
-                    content: "";
-                    position: absolute;
-                    top: 0;
-                    right: 0;
-                    bottom: 0;
-                    left: 0;
-                    z-index: 2;
-
-                    opacity: 0.05;
-                    background: linear-gradient(var(--clr-900), var(--clr-100) 90%);
-                }
-
-                .bit {
-                    position: relative;
-                    width: 100%;
-
-                    padding: 0 32%;
-
-                    // transform: translateX(calc(50% - (var(--scaleX) / 2) * 100%));
-
-                    :global(.illustration) {
-                        transform: translateX(calc(50% - (var(--scaleX) / 2) * 100%));
-                    }
-
-                    &__bottom {
-                        position: relative;
-                    }
-
-                    .flutes {
-                        position: absolute;
-                        top: 0;
-                        right: 0;
-                        bottom: 0;
-                        left: 0;
-
-                        overflow: hidden;
-
-                        &__inner {
-                            position: relative;
-                            animation: moveUp 2s linear infinite;
-                        }
-                    }
-                }
-            }
         }
     }
 
@@ -216,11 +114,5 @@
                 max-height: unset;
             }
         }
-    }
-
-    /* === ANIMATIONS ========================= */
-    @keyframes moveUp {
-        from { transform: translateY(0%); }
-        to { transform: translateY(calc(-50% / var(--flutes))); }
     }
 </style>
