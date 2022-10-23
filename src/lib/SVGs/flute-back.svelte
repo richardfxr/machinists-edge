6b029321bb17d176dea9a29d200037fd747122c6
Richard Fu <richardfxr.personal@gmail.com>
2022-10-22 21:30:26 -0400

    🟠SVLT: removed incorrect path from flute-back

--- src/lib/SVGs/flute-back.svelte
+++ src/lib/SVGs/flute-back.svelte
@@ -20,7 +20,6 @@
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 81.5 230">
     <path class="fill-100" transform="scale({scaleX}, {scaleY})" d="M.75,0C.75,75,62.25,155,62.25,230h18.5C80.75,155,19.25,75,19.25,0"/>
-    <path class="stroke default" vector-effect="non-scaling-stroke" transform="scale({scaleX}, {scaleY})" d="M.75,0C.75,75,80.75,155,80.75,230"/>
     <path class="stroke default" vector-effect="non-scaling-stroke" transform="scale({scaleX}, {scaleY})" d="M.75,0C.75,75,62.25,155,62.25,230"/>
     <path class="stroke default" vector-effect="non-scaling-stroke" transform="scale({scaleX}, {scaleY})" d="M19.25,0c0,75,61.5,155,61.5,230"/>
 </svg>
