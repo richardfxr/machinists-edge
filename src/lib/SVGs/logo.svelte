152a8e3801def056724118e8f819674be13121f1
Richard Fu <richardfxr.personal@gmail.com>
2022-10-23 00:38:35 -0400

    🟠SVLT: created logo SVG

---
+++ src/lib/SVGs/logo.svelte
@@ -0,0 +1,16 @@
+<svg
+    class="illustration logo"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 103 61">
+    <polygon class="fill-800" points="46.36 0 54 15 103 15 103 0 46.36 0"/>
+    <polygon class="fill-800" points="20.55 0 51.63 61 68.46 61 37.38 0 20.55 0"/>
+    <polygon class="fill-800" points="58.08 23 65.72 38 95 38 95 23 58.08 23"/>
+    <polygon class="fill-800" points="69.8 46 77.44 61 103 61 103 46 69.8 46"/>
+    <polygon class="fill-800" points="0 0 0 10.33 25.82 61 42.65 61 11.57 0 0 0"/>
+    <polygon class="fill-800" points="0 60.99 0 61 16.84 61 0 27.95 0 60.99"/>
+</svg>
+
+
+<style lang="scss">
+    
+</style>
