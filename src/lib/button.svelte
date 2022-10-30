5fe6a661335d244d02d9e5e0c870c488eacf3b6b
Richard Fu <richardfxr.personal@gmail.com>
2022-10-30 15:05:12 -0400

    🟠SVLT: added button component

---
+++ src/lib/button.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    /* === PROPS ============================== */
+    export let type: "text" | "text--uppercase" = "text";
+</script>
+
+
+<button class="button {type}" on:click>
+    <slot></slot>
+</button>
+
+
+<style lang="scss">
+    .button {
+        color: var(--clr-800);
+        font-size: var(--font-lg);
+        font-weight: 500;
+        line-height: 1em;
+
+        padding: var(--pad-2xs) var(--pad-lg);
+        border: var(--border) var(--clr-300);
+
+        transition: color var(--trans-fast),
+                    border-color var(--trans-fast);
+        
+        &:hover, &:focus-visible {
+            color: var(--clr-900);
+            border-color: var(--clr-800);
+        }
+
+        &.text--uppercase {
+            font-weight: 600;
+            text-transform: uppercase;
+
+            padding: var(--pad-xs) var(--pad-xl);
+        }
+    }
+</style>
