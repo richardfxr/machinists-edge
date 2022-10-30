7a18551877d4ec47ecec595d9a4fe001069bffd3
Richard Fu <richardfxr.personal@gmail.com>
2022-10-30 15:06:18 -0400

    🟠SVLT: added footer quickSettings icons

---
+++ src/lib/SVGs/motion-full.svelte
@@ -0,0 +1,12 @@
+<svg
+    id="motion-full"
+    class="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 40 27">
+    <rect y="3.5" width="1" height="20"/>
+    <rect x="3" y="3.5" width="2" height="20"/>
+    <rect x="7" y="3.5" width="4" height="20"/>
+    <rect x="13" y="3.5" width="6" height="20"/>
+    <rect x="21" y="3.5" width="8" height="20"/>
+    <rect x="31" y="3.5" width="9" height="20"/>
+</svg>
---
+++ src/lib/SVGs/motion-low.svelte
@@ -0,0 +1,8 @@
+<svg
+    id="motion-low"
+    class="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 40 27">
+    <rect y="3.5" width="5" height="20"/>
+    <rect x="7" y="3.5" width="33" height="20"/>
+</svg>
