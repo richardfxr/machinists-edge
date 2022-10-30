7b317172fcd4e67336fb506dfe28fc379e234456
Richard Fu <richardfxr.personal@gmail.com>
2022-10-30 15:05:55 -0400

    🟠SVLT: extract toolTip component from unitAbbr

---
+++ src/lib/toolTip.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    /* === PROPS ============================== */
+    export let position: "top-left" | "top-right" | "bottom-left" | "bottom-right" = "top-left";
+</script>
+
+
+<tool-tip class="toolTip {position}" role="tooltip">
+    <slot></slot>
+</tool-tip>
+
+
+<style lang="scss">
+    .toolTip {
+        pointer-events: none;
+        user-select: none;
+        position: absolute;
+        z-index: 1;
+        inline-size: max-content;
+
+        color: var(--clr-900);
+        font-weight: 400;
+
+        padding: var(--pad-3xs) var(--pad-xs);
+        background-color: var(--clr-200);
+
+        opacity: 0;
+        transition: opacity var(--trans-fast);
+
+        &.top-left {
+            right: 0;
+            bottom: 120%;
+        }
+
+        &.top-right {
+            left: 0;
+            bottom: 120%;
+        }
+
+        &.bottom-left {
+            right: 0;
+            top: 120%;
+        }
+
+        &.bottom-right {
+            left: 0;
+            top: 120%;
+        }
+    }
+</style>
--- src/lib/unitAbbr.svelte
+++ src/lib/unitAbbr.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import ToolTip from "./toolTip.svelte";
+
     /* === PROPS ============================== */
     export let units: "RPM"| "IPM" | "in" | "SFPM" | "IPR";
     export let position: "top-left" | "top-right" | "bottom-left" | "bottom-right" = "top-left";
@@ -31,7 +33,7 @@
 
 <abbr class="unitAbbr">
     {units}
-    <tool-tip class="toolTip {position}" role="tooltip">{fullName}</tool-tip>
+    <ToolTip {position}>{fullName}</ToolTip>
 </abbr>
 
 
@@ -53,45 +55,8 @@
         &:hover, &:focus-visible, &:active {
             color: var(--_clr-hover);
 
-            .toolTip {
+            :global(.toolTip) {
                 opacity: 1;
-            }
-        }
-
-        .toolTip {
-            pointer-events: none;
-            user-select: none;
-            position: absolute;
-            z-index: 1;
-            inline-size: max-content;
-
-            color: var(--clr-900);
-            font-weight: 400;
-
-            padding: var(--pad-3xs) var(--pad-xs);
-            background-color: var(--clr-200);
-
-            opacity: 0;
-            transition: opacity var(--trans-fast);
-
-            &.top-left {
-                right: 0;
-                bottom: 120%;
-            }
-
-            &.top-right {
-                left: 0;
-                bottom: 120%;
-            }
-
-            &.bottom-left {
-                right: 0;
-                top: 120%;
-            }
-
-            &.bottom-right {
-                left: 0;
-                top: 120%;
             }
         }
     }
