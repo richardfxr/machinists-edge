a1ae767c4316f8999a1d17ac7b77edff6b2e0e7e
Richard Fu <richardfxr.personal@gmail.com>
2022-11-02 22:21:12 -0400

    🟠SVLT: added saveLoader icons

---
+++ src/lib/SVGs/deleteSave.svelte
@@ -0,0 +1,11 @@
+<svg
+    id="deleteSave"
+    class="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 30 30">
+    <polygon points="0 25.56 25.56 0 18.49 0 0 18.49 0 25.56"/>
+    <polygon points="18.19 30 30 18.19 30 11.12 11.12 30 18.19 30"/>
+    <polygon points="14.25 0 7.5 0 0 7.5 0 14.25 14.25 0"/>
+    <polygon points="22.44 30 30 30 30 22.43 22.44 30"/>
+    <polygon points="30 6.88 30 0 29.81 0 0 29.81 0 30 6.88 30 30 6.88"/>
+</svg>
---
+++ src/lib/SVGs/newSave.svelte
@@ -0,0 +1,7 @@
+<svg
+    id="newSave"
+    class="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 30 30">
+    <path d="M26.97,0V2.25c0,.83-.67,1.5-1.5,1.5s-1.5-.67-1.5-1.5V0H7.5L0,7.5V30H30V0h-3.03Zm-2.97,16.5h-7.5v7.5h-3v-7.5H6v-3h7.5V6h3v7.5h7.5v3Z"/>
+</svg>
