02bc3751bf2efcc0038ad6a5a2ad704059cbf804
Richard Fu <richardfxr.personal@gmail.com>
2022-11-17 09:54:02 -0500

    🚧WIP: testing page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
     /* === IMPORTS ============================ */
+    import { page } from '$app/stores';
+    import { fade } from 'svelte/transition';
     import { colorTheme } from "../store/store";
     import Header from "$lib/header.svelte";
     import Footer from "$lib/footer.svelte";
+    import { motionPref } from "../store/store";
+
+    /* === TYPES ============================== */
+    interface animationOptions {
+        animation: Function;
+        duration: number;
+        y?: number;
+        delay?: number;
+    }
+
+    /* === FUNCTIONS ========================== */
+    function animate(node: HTMLElement, options: animationOptions) {
+        if ($motionPref === "reduced")
+            return fade(node, { duration: 200 });
+        else
+            return options.animation(node, options);
+    }
 </script>
 
 
@@ -13,15 +32,33 @@
 
 <Header />
 
-<main id="main">
-    <slot></slot>
-</main>
+<!-- page transition -->
+<div class="pageTransition__container">
+    {#key $page.url.pathname}
+        <main
+            id="main"
+            in:animate={{ animation: fade, duration: 300, delay: 1000 }}
+            out:animate={{ animation: fade, duration: 300 }}>
+            <slot></slot>
+        </main>
+    {/key}
+</div>
 
 <Footer />
 
 
 <style lang="scss">
+    .pageTransition__container {
+        // wrapper grid to prevent scrollbar from appearing during page transitions
+        display: grid;
+        grid-template: 1fr / 1fr;
+    }
+    
     #main {
+        // forces #main into wrapper grid to prevent scrollbar
+        grid-row: 1;
+        grid-column: 1;
+        width: 100%;
         max-width: var(--max-width);
 
         padding: 0 var(--pad-hrz);
