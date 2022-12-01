86ad569c7dbc0f800afe35c5cd4ac5c270b778fc
Richard Fu <richardfxr.personal@gmail.com>
2022-12-01 16:15:48 -0500

    🟠SVLT: added placeholder to all unfinished pages

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
     /* === IMPORTS ============================ */
     import Heading from "$lib/heading.svelte";
+    import Placeholder from "$lib/placeholder.svelte";
 </script>
 
 
 <Heading>Settings</Heading>
+
+<div class="page">
+    <Placeholder />
+</div>
 
 
 <style lang="scss">
--- src/routes/tool-length/+page.svelte
+++ src/routes/tool-length/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
     /* === IMPORTS ============================ */
     import Heading from "$lib/heading.svelte";
+    import Placeholder from "$lib/placeholder.svelte";
 </script>
 
 
 <Heading>Tool Length Calculator</Heading>
+
+<div class="page">
+    <Placeholder />
+</div>
 
 
 <style lang="scss">
